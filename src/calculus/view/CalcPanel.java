dd49ce2408b25e6e98e181f86b52a1926c86df12
Bowen <tbow1382@740s-pl-120.local>
2019-04-09 08:29:01 -0600

    Instantiated and declared data members in CalcPanel.java.

--- src/calculus/view/CalcPanel.java
+++ src/calculus/view/CalcPanel.java
@@ -10,18 +10,69 @@ public class CalcPanel extends JPanel
 	
 	private CalculusController theController;
 	private SpringLayout theLayout;
+	//////////FOUR BASIC OPERATIONS/////////
 	private JButton multiplyButton;
-	private JButton add;
-	private JButton subtract;
-	private JButton divide;
-	private JButton logarithm; //This is supposed to be for ordinary logarithm operations -GET BETTER NAME FOR VARIABLE-
+	private JButton addButton;
+	private JButton subtractButton;
+	private JButton divideButton;
+	///////EXPONENTIALS/////////////
+	private JButton logButton; //This is supposed to be for ordinary logarithm operations
 	private JButton raiseByPower; //This is supposed to be for exponentials - GET A BETTER NAME FOR THE VARIABLE-
 	private JButton logarithmNaturale; //This is for the ln button AKA the natural logarithm button -SHORTER NAME PLEASE!-
+	
+	private JButton decimalPoint; //This allows the user to use decimal numbers
+	private JButton leftParenthesis; //This is a left paren
+	private JButton rightParenthesis; //This is a right paren
+	///////NUMBER BUTTONS///////
+	private JButton zero;
+	private JButton one;
+	private JButton two;
+	private JButton three;
+	private JButton four;
+	private JButton five;
+	private JButton six;
+	private JButton seven;
+	private JButton eight;
+	private JButton nine;
+	
+	
+	
 	
 	public CalcPanel(CalculusController theController)
 	{
 		super();
 		this.theController = theController;
+		this.theLayout = new SpringLayout();
+		
+		this.multiplyButton = new JButton("*");
+		this.addButton = new JButton("+");
+		this.subtractButton = new JButton("-");
+		this.divideButton = new JButton("/");
+		
+		this.logButton = new JButton("log");
+		this.raiseByPower = new JButton("^");
+		this.logarithmNaturale = new JButton("ln");
+		
+		this.decimalPoint = new JButton(".");
+		this.leftParenthesis = new JButton("(");
+		this.rightParenthesis = new JButton(")");
+		
+		this.zero = new JButton("0");
+		this.one = new JButton("1");
+		this.two = new JButton("2");
+		this.three = new JButton("3");
+		this.four = new JButton("4");
+		this.five = new JButton("5");
+		this.six = new JButton("6");
+		this.seven = new JButton("7");
+		this.eight = new JButton("8");
+		this.nine = new JButton("9");
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 		
 	}
 	
