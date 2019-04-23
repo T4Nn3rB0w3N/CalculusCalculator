24e428a4c3d01acdbcb5585797517ec1809e12d7
Bowen <tbow1382@740s-pl-120.local>
2019-04-23 08:32:48 -0600

    Got the data members setup as a window.

--- src/calculus/view/CalcPanel.java
+++ src/calculus/view/CalcPanel.java
@@ -113,7 +113,50 @@ public class CalcPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		theLayout.putConstraint(SpringLayout.WEST, clearButton, 0, SpringLayout.WEST, enterButton);
+		theLayout.putConstraint(SpringLayout.SOUTH, clearButton, -6, SpringLayout.NORTH, multiplyButton);
+		theLayout.putConstraint(SpringLayout.WEST, multiplyButton, 0, SpringLayout.WEST, enterButton);
+		theLayout.putConstraint(SpringLayout.WEST, addButton, 0, SpringLayout.WEST, enterButton);
+		theLayout.putConstraint(SpringLayout.SOUTH, addButton, -6, SpringLayout.NORTH, subtractButton);
+		theLayout.putConstraint(SpringLayout.WEST, subtractButton, 0, SpringLayout.WEST, enterButton);
+		theLayout.putConstraint(SpringLayout.SOUTH, multiplyButton, -6, SpringLayout.NORTH, divideButton);
+		theLayout.putConstraint(SpringLayout.WEST, divideButton, 0, SpringLayout.WEST, enterButton);
+		theLayout.putConstraint(SpringLayout.NORTH, divideButton, 0, SpringLayout.NORTH, logButton);
+		theLayout.putConstraint(SpringLayout.WEST, logButton, 0, SpringLayout.WEST, this);
+		theLayout.putConstraint(SpringLayout.NORTH, expButton, 6, SpringLayout.SOUTH, logButton);
+		theLayout.putConstraint(SpringLayout.EAST, expButton, 0, SpringLayout.EAST, logButton);
+		theLayout.putConstraint(SpringLayout.NORTH, lnButton, 6, SpringLayout.SOUTH, expButton);
+		theLayout.putConstraint(SpringLayout.NORTH, subtractButton, 0, SpringLayout.NORTH, lnButton);
+		theLayout.putConstraint(SpringLayout.EAST, lnButton, 0, SpringLayout.EAST, logButton);
+		theLayout.putConstraint(SpringLayout.NORTH, enterButton, 0, SpringLayout.NORTH, decimalPoint);
+		theLayout.putConstraint(SpringLayout.NORTH, leftParenthesis, 116, SpringLayout.NORTH, this);
+		theLayout.putConstraint(SpringLayout.NORTH, logButton, 0, SpringLayout.SOUTH, leftParenthesis);
+		theLayout.putConstraint(SpringLayout.EAST, leftParenthesis, 0, SpringLayout.EAST, logButton);
+		theLayout.putConstraint(SpringLayout.NORTH, rightParenthesis, 116, SpringLayout.NORTH, this);
+		theLayout.putConstraint(SpringLayout.WEST, rightParenthesis, 8, SpringLayout.EAST, leftParenthesis);
+		theLayout.putConstraint(SpringLayout.WEST, enterButton, 87, SpringLayout.EAST, zero);
+		theLayout.putConstraint(SpringLayout.WEST, zero, 230, SpringLayout.WEST, this);
+		theLayout.putConstraint(SpringLayout.EAST, decimalPoint, -6, SpringLayout.WEST, zero);
+		theLayout.putConstraint(SpringLayout.NORTH, decimalPoint, 6, SpringLayout.SOUTH, one);
+		theLayout.putConstraint(SpringLayout.EAST, one, 0, SpringLayout.EAST, decimalPoint);
+		theLayout.putConstraint(SpringLayout.NORTH, zero, 6, SpringLayout.SOUTH, two);
+		theLayout.putConstraint(SpringLayout.WEST, two, 0, SpringLayout.WEST, zero);
+		theLayout.putConstraint(SpringLayout.WEST, three, 6, SpringLayout.EAST, two);
+		theLayout.putConstraint(SpringLayout.NORTH, one, 6, SpringLayout.SOUTH, four);
+		theLayout.putConstraint(SpringLayout.WEST, four, 0, SpringLayout.WEST, decimalPoint);
+		theLayout.putConstraint(SpringLayout.NORTH, two, 6, SpringLayout.SOUTH, five);
+		theLayout.putConstraint(SpringLayout.WEST, five, 0, SpringLayout.WEST, zero);
+		theLayout.putConstraint(SpringLayout.NORTH, three, 6, SpringLayout.SOUTH, six);
+		theLayout.putConstraint(SpringLayout.EAST, six, 0, SpringLayout.EAST, three);
+		theLayout.putConstraint(SpringLayout.NORTH, seven, 0, SpringLayout.SOUTH, rightParenthesis);
+		theLayout.putConstraint(SpringLayout.NORTH, four, 6, SpringLayout.SOUTH, seven);
+		theLayout.putConstraint(SpringLayout.EAST, seven, 0, SpringLayout.EAST, decimalPoint);
+		theLayout.putConstraint(SpringLayout.NORTH, eight, 145, SpringLayout.NORTH, this);
+		theLayout.putConstraint(SpringLayout.NORTH, five, 6, SpringLayout.SOUTH, eight);
+		theLayout.putConstraint(SpringLayout.EAST, eight, 0, SpringLayout.EAST, zero);
+		theLayout.putConstraint(SpringLayout.NORTH, nine, 145, SpringLayout.NORTH, this);
+		theLayout.putConstraint(SpringLayout.NORTH, six, 6, SpringLayout.SOUTH, nine);
+		theLayout.putConstraint(SpringLayout.EAST, nine, 0, SpringLayout.EAST, three);
 	}
 	
 	private void setupListeners()
